af508ab8b85b48508c244c1e4fc295384e989672
Stefaney <sfg239@nyu.edu>
2014-08-12 16:58:24 -0400

    2nd commit

---
+++ Assets/Standard Assets/MyScripts/Enemy_Move_Toward_Player.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Move_Toward_Player : MonoBehaviour {
+
+	float max_distance_from_player = 10f; 
+	float distance_from_player_squared = 100f;
+	GameObject player;
+	float rotationSpeed = 5f;
+	float moveSpeed = 4f;
+	// Use this for initialization
+	void Start () {
+
+		 player = GameObject.FindGameObjectWithTag("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		Vector3 ray_direction = player.transform.localPosition - transform.localPosition;
+
+		if ( ray_direction.sqrMagnitude < distance_from_player_squared)
+		{
+
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, ray_direction, out hit, max_distance_from_player) && (hit.collider.gameObject==player) )
+			{
+
+				transform.rotation = Quaternion.Slerp(transform.rotation,
+				Quaternion.LookRotation(player.transform.position - transform.position), rotationSpeed*Time.deltaTime);
+
+				transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+			}
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider collide) {
+		if (collide.tag == "Light")
+		{
+
+			moveSpeed = 0f;
+		}
+
+	
+	}
+
+	void OnTriggerExit(Collider collide)
+	{
+		if (collide.tag == "Light")
+		{
+		 moveSpeed = 4f;
+		}
+	}
+}
