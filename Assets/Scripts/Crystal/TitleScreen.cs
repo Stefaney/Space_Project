722f80cbc3ae0d6aa47c0c4d9328913cb519afac
Man Ching Lam (Crystal) <manchinglam@172-26-42-46.dynapool.nyu.edu>
2014-08-14 16:06:04 -0400

    Final

---
+++ Assets/Scripts/Crystal/TitleScreen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+	public Texture background;
+	
+	void OnGUI (){
+		
+		GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height), background);
+		GUI.Box (new Rect(750, 450, 300, 280), "Main Menu");
+		
+		if (GUI.Button (new Rect (820, 500, 150, 30), "Play Game")) {
+			Application.LoadLevel (1);
+		}
+		
+		if (GUI.Button (new Rect (820, 550, 150, 30), "Story")) {
+			Application.LoadLevel (2);
+		}
+		
+		if(GUI.Button (new Rect(820,600,150,30), "How To Play")){
+			Application.LoadLevel (3);			
+		}
+		
+		if(GUI.Button (new Rect(820,650,150,30), "Credits")){
+			Application.LoadLevel (4);			
+		}
+	}
+}
