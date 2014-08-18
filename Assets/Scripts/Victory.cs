81280ba39b11e288c71efa7c000659049e2cd52a
Kristin Ho <kh1550@nyu.edu>
2014-08-18 17:00:32 -0400

    final final commit: victory screen

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Victory : MonoBehaviour {
+	void OnGUI (){
+		if (GUI.Button (new Rect (375, 375, 150, 30), "Play again")) {
+			Application.LoadLevel (1);
+		}
+		
+		if (GUI.Button (new Rect (375, 425, 150, 30), "Main Menu")) {
+			Application.LoadLevel (0);
+		}
+	}
+}
